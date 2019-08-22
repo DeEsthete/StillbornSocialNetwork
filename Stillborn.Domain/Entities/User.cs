3206b19c43de46ee4b3cedfc2515f4c2d5d38ffb
JekaLion <djoni-01@mail.ru>
2019-08-22 19:55:05 +0600

    Apchi

--- Stillborn.Domain/Entities/User.cs
+++ Stillborn.Domain/Entities/User.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿//using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Stillborn.Domain.Entities
 {
-    public class User : IdentityUser
+    public class User //: IdentityUser
     {
         public string NickName { get; set; }
         public bool IsMan { get; set; }
