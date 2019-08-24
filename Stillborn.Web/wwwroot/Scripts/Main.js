e1d228b1ca0118c31b73fef52b648499190a169c
JeLion <djoni-01@mail.ru>
2019-08-24 12:48:47 +0600

    add simple js file

---
+++ Stillborn.Web/wwwroot/Scripts/Main.js
@@ -0,0 +1,18 @@
+﻿//#region Main
+var mainUrl = "";
+
+function Ajax(urlAppend, type, success, data, error) {
+    $.Ajax({
+        url: mainUrl + urlAppend,
+        type: type,
+        data: data,
+        success: success,
+        error: error
+    });
+}
+//#endregion
+
+//#region UserProfile
+function DrawUserProfile(user) {
+}
+//#endregion
