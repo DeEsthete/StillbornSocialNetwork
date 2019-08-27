c34a907bc41063cf4e135422235391e06410bd03
Yernar <yernar8m@gmail.com>
2019-08-27 09:21:03 +0600

    update initializer

--- Stillborn.Domain/Data/StillbornContext.cs
+++ Stillborn.Domain/Data/StillbornContext.cs
@@ -15,24 +15,24 @@ namespace Stillborn.Domain.Data
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=StillbornDb;Trusted_Connection=True;");
         }
         //сортировано по иерархии вложенности
-        public DbSet<MediaType> MediaTypes { get; set; }//*
-        public DbSet<Content> Content { get; set; }//* Путает понимание значения, лучше Content
-        public DbSet<Media> Media { get; set; }//* это Media
+        public DbSet<MediaType> MediaTypes { get; set; }
+        public DbSet<Content> Content { get; set; }
+        public DbSet<Media> Media { get; set; }
         //*************
-        public DbSet<Wall> Walls { get; set; }//*
-        public DbSet<Post> Posts { get; set; }//* посты обычно должны знать у какого юзера находятся
-        public DbSet<Comment> Comments { get; set; }//* Text по идее тоже Media
+        public DbSet<Wall> Walls { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
         //*************
-        public DbSet<ChatRoom> ChatRooms { get; set; }//нужен UserId-
-        public DbSet<Message> Messages { get; set; }//ему не нужно знать в каком Chatroom -
+        public DbSet<ChatRoom> ChatRooms { get; set; }
+        public DbSet<Message> Messages { get; set; }
         //*************
-        public DbSet<ContactType> ContactTypes { get; set; }//*
-        public DbSet<GroupRole> GroupRoles { get; set; }//*
+        public DbSet<ContactType> ContactTypes { get; set; }
+        public DbSet<GroupRole> GroupRoles { get; set; }
         public DbSet<Group> Groups { get; set; }
-        public DbSet<Member> Members { get; set; } // нужен UserId -
+        public DbSet<Member> Members { get; set; } 
 
-        public DbSet<UserChatRoom> UserChatRooms { get; set; }//нужен UserId-
-        public DbSet<UserContact> UserContacts { get; set; }//нужен UserId-
+        public DbSet<UserChatRoom> UserChatRooms { get; set; }
+        public DbSet<UserContact> UserContacts { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder builder)
--- Stillborn.Domain/Data/StillbornDBInitializer.cs
+++ Stillborn.Domain/Data/StillbornDBInitializer.cs
@@ -13,66 +13,40 @@ namespace Stillborn.Domain.Data
             modelBuilder.Entity<MediaType>().HasData(
                 new MediaType() { Id = 1, Name = "Image", CreationDate = GetCreationDateTime() },
                 new MediaType() { Id = 2, Name = "Music", CreationDate = GetCreationDateTime() },
-                new MediaType() { Id = 3, Name = "Video", CreationDate = GetCreationDateTime() },
-                new MediaType() { Id = 4, Name = "Text", CreationDate = GetCreationDateTime() }
+                new MediaType() { Id = 3, Name = "Video", CreationDate = GetCreationDateTime() }
+                //new MediaType() { Id = 4, Name = "Text", CreationDate = GetCreationDateTime() } // на случай если поменяем Text как Media
                 );
-
-            modelBuilder.Entity<Content>().HasData(
-                new Content() { Id = 1, CreationDate = GetCreationDateTime() },
-                new Content() { Id = 2, CreationDate = GetCreationDateTime() },
-                new Content() { Id = 3, CreationDate = GetCreationDateTime() },
-                new Content() { Id = 4, CreationDate = GetCreationDateTime() }
-                );
-
-            modelBuilder.Entity<Wall>().HasData(
-                new Wall() { Id = 1, CreationDate = GetCreationDateTime() },
-                new Wall() { Id = 2, CreationDate = GetCreationDateTime() },
-                new Wall() { Id = 3, CreationDate = GetCreationDateTime() }
-                ); ;
 
             modelBuilder.Entity<ContactType>().HasData(
-                new ContactType() { Id = 1, Name = "Friend" },
-                new ContactType() { Id = 2, Name = "Follower" }
+                new ContactType() { Id = 1, Name = "Friend", CreationDate=GetCreationDateTime() },
+                new ContactType() { Id = 2, Name = "Follower", CreationDate=GetCreationDateTime() }
                 );
 
             modelBuilder.Entity<GroupRole>().HasData(
                 new GroupRole() { Id = 1, Name = "Admin", CreationDate = GetCreationDateTime() },
                 new GroupRole() { Id = 2, Name = "Subscriber", CreationDate = GetCreationDateTime() }
                 );
-            //лучше вместо имени Content использовать Media
-            //modelBuilder.Entity<Media>().HasData(
-            //    new Media() { ContentId = 1, CreationDate = GetRandomCreationDateTime(), Likes = 2, TypeId = 1 },
-            //    new Media() {  ContentId = 2, CreationDate = GetRandomCreationDateTime(), Likes = 3, TypeId = 2 },
-            //    new Media() {  ContentId = 3, CreationDate = GetRandomCreationDateTime(), Likes = 4, TypeId = 3 }
-            //    );
+
+            //Остальное побочное добавим после реализации их добавления
             //**************
-
-            //modelBuilder.Entity<Post>().HasData(
-            //    new Post() { Id=1, WallId = 1, CreationDate = GetRandomCreationDateTime(), Text = "First Post", Likes = 1},
-            //    new Post() { Id=2, WallId =1, CreationDate = GetRandomCreationDateTime(), Text = "Second Post", Likes = 15},
-            //    new Post() { Id=3, WallId = 3, CreationDate = GetRandomCreationDateTime(), Text = "Third Post", Likes = 1}
-            //    );
-
-            //modelBuilder.Entity<Comment>().HasData(
-            //    new Comment() { Id =1, ContentId = 1, CreationDate = GetRandomCreationDateTime(), Text = "First Comment", Likes = 1},
-            //    new Comment() {  Id =2, ContentId = 3, CreationDate = GetRandomCreationDateTime(), Text = "Second Comment", Likes = 1},
-            //    new Comment() {  Id =3, ContentId = 2, CreationDate = GetRandomCreationDateTime(), Text = "Third Comment", Likes = 1}
-            //    );
-
-            //modelBuilder.Entity<Message>().HasData(
-            //    new Message() { Id = 1, CreationDate = GetRandomCreationDateTime() },
-            //    new Message() { Id = 2, CreationDate = GetRandomCreationDateTime() },
-            //    new Message() { Id = 3, CreationDate = GetRandomCreationDateTime() },
-            //    new Message() { Id = 4, CreationDate = GetRandomCreationDateTime() }
-            //    );
-
-
+            //Content
+            //Media
+            //Wall
+            //Post
+            //Comment
+            //ChatRoom
+            //Message
+            //Group
+            //Member
+            //UserChatRoom
+            //UserContact
         }
 
         private static DateTime GetCreationDateTime()
         {
             Random random = new Random();
-            var date = DateTime.UtcNow.Date.AddDays(random.Next(-365, 0));
+            int daysAgo = random.Next(-365*2, 0);
+            var date = DateTime.UtcNow.AddDays(daysAgo);
             return date;
         }
     }
