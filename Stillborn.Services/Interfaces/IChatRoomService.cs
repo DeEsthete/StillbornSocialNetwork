c6aadb1a646c8ea58fcffb6cd84eb8b3c5a667cf
UkiOnna <sikirovtemirlan@mail.ru>
2019-08-29 13:52:39 +0600

    Добавил Контент сервис и ЧатРум сервис(возможно нуждается в доработке)

---
+++ Stillborn.Services/Interfaces/IChatRoomService.cs
@@ -0,0 +1,19 @@
+﻿using Stillborn.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stillborn.Services.Interfaces
+{
+    public interface IChatRoomService
+    {
+        bool AddUserToChatRoom(string userId, int chatGroupId);
+        bool LeaveChatRoom(string userId, int chatGroupId);
+        bool CreateChatRoom(string creatorId, string name);
+        bool DeleteChatRoom(int chatGroupId);
+        IEnumerable<User> GetChatRoomUsers(int chatRoomId);
+        IEnumerable<Message> GetChatRoomMessages(int chatRoomId);
+        bool SendMessage(int senderId,int contentId);
+
+    }
+}
