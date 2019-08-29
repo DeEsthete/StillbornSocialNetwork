26e4b55cd78d7d6d7dc3305fbad9e27201f04f2c
Yernar <yernar8m@gmail.com>
2019-08-29 14:08:43 +0600

    add hosted service

---
+++ Stillborn.Services/Services/DBFillHostedService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stillborn.Services.Services
+{
+    public class DBFillHostedService : IHostedService, IDisposable
+    {
+        private readonly Repositories.RepositoryService _repositoryService;
+
+        public DBFillHostedService(Repositories.RepositoryService repositoryService)
+        {
+            _repositoryService = repositoryService;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
